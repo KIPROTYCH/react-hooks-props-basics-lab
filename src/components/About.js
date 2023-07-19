428580a606785ac9f76530e47d98905e6964a8f2
KIPROTYCH <kiprotych@gmail.com>
2023-07-19 15:48:46 +0300

    Completed Assignment

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,9 @@ function About() {
       <h2>About Me</h2>
       <p>Put the bio in here</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <a href="https://github.com/liza">Visit Github</a>
+      <a href="https://ke.linkedin.com/in/liza">Visit Github</a>
+
     </div>
   );
 }
